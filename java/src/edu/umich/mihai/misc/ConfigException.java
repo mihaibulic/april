714b1b396bc68b584ffb9c1d9c16ebb3e903298b
APRIL Laboratory <april@april.eecs.umich.edu>
2011-05-09 16:32:49 -0400

    forgot to commit some stuff

---
+++ java/src/edu/umich/mihai/misc/ConfigException.java
@@ -0,0 +1,29 @@
+package edu.umich.mihai.misc;
+
+public class ConfigException extends Exception
+{
+	private static final long serialVersionUID = 1L;
+	
+	public static final int INDICES_URL_LENGTH = 1;
+	public static final int NULL_CONFIG = 2;
+	
+	public ConfigException(int error)
+	{
+		String msg = "Config Exception: ";
+		
+		switch(error)
+		{
+			case INDICES_URL_LENGTH:
+				msg += "Number of camera URLs and indices do not match";
+				break;
+			case NULL_CONFIG:
+				msg += "Config file is null";
+				break;	
+        	default:
+        		msg += "Unknown error";
+		}
+    
+	    System.err.println(msg);
+	    System.exit(1);
+	}
+}
---
+++ java/src/edu/umich/mihai/misc/Util.java
@@ -0,0 +1,11 @@
+package edu.umich.mihai.misc;
+
+import april.config.Config;
+
+public class Util
+{
+    public static void verifyConfig(Config config) throws ConfigException
+    {
+        if(config == null || !config.getBoolean("valid", false)) throw new ConfigException(ConfigException.NULL_CONFIG);
+    }
+}
